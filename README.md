275087f1207d934edc07588833aa6e73f32d59a6
jhmarkkk <junhaomark@gmail.com>
2022-02-07 22:28:50 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# site_env
+ heroku personal website
---
+++ site_main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html', title='Homepage', active_nav='home_nav')
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About', active_nav='about_nav')
+
+if __name__ == '__main__':
+    app.run(debug=True)
